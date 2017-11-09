93e96bfaa03a90a2bf76d364ebdbe468947ef35d
Aleksei Gutikov <aleksey.gutikov@synesis.ru>
2017-11-09 11:38:01 +0300

    q

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -10,8 +10,7 @@
     ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" default)))
  '(package-selected-packages
    (quote
-    (color-theme-sanityinc-tomorrow molokai-theme markdown-mode helm-fuzzy-find material-theme solarized-theme monokai-theme zenburn-theme apt-sources-list cyberpunk-theme powerline nlinum zygospore helm-gtags helm yasnippet volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu)))
-)
+    (color-theme-sanityinc-tomorrow molokai-theme markdown-mode helm-fuzzy-find material-theme solarized-theme monokai-theme zenburn-theme apt-sources-list cyberpunk-theme powerline nlinum zygospore helm-gtags helm yasnippet volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
